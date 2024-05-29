a4a765226af9342b2aa2179ea3adb3dc9658b7de
moiup <mohammedbashir.m@gmail.com>
2024-05-29 22:51:55 +0200

    Removing default changeLayout in create method. Must be precised by the user.

--- IgniteEngine/IgniteEngine/Texture.cpp
+++ IgniteEngine/IgniteEngine/Texture.cpp
@@ -95,8 +95,7 @@ void Texture::create() {
 		VK_IMAGE_USAGE_TRANSFER_DST_BIT |
 		VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
 		VK_IMAGE_USAGE_SAMPLED_BIT |
-		VK_IMAGE_USAGE_STORAGE_BIT |
-		VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
+		VK_IMAGE_USAGE_STORAGE_BIT
 	);
 	Image::setMemoryPropertyFlags(VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
@@ -115,7 +114,6 @@ void Texture::create() {
 	);
 	Image::createImageView();
 
-	changeLayout(VK_IMAGE_LAYOUT_GENERAL);
 
 	// Creating the staging buffer
 	_staging_buffer.setQueue(_queue);

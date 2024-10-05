3129d341c04099bfcf77ae8de643440995c9e0d0
Moiup <mohammedbashir.m@gmail.com>
2024-10-05 22:54:51 +0200

    Print method.

--- IgniteEngine/IgniteEngine/src/Skeleton.cpp
+++ IgniteEngine/IgniteEngine/src/Skeleton.cpp
@@ -8,10 +8,28 @@ const Joint* Skeleton::skeleton() const {
 	return _skeleton;
 }
 
+Joint* Skeleton::skeleton() {
+	return _skeleton;
+}
+
 std::vector<Joint>& Skeleton::joints() {
 	return _joints;
 }
 
-//const std::vector<Joint>& Skeleton::joints() const {
-//	return _joints;
-//}
+const std::vector<Joint>& Skeleton::joints() const {
+	return _joints;
+}
+
+std::ostream& operator<<(std::ostream& os, const Skeleton& s){
+	os << "skeleton (nb joints = " + std::to_string(s.joints().size()) + "):" << " ";
+
+	os << "[";
+	for (const Joint& j : s.joints()) {
+		os << " " << std::to_string(j.id());
+	}
+	os << " ]\n";
+
+	os << s.skeleton()->toString(true);
+
+	return os;
+}

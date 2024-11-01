1acc714e16347a959017dd99265d76e614abe2f5
Moiup <mohammedbashir.m@gmail.com>
2024-11-01 13:02:40 +0100

    Removing created boolean. Making destoy private and adding copy constructor and destructor.

--- IgniteEngine/IgniteEngine/inc/CommandPool.h
+++ IgniteEngine/IgniteEngine/inc/CommandPool.h
@@ -5,22 +5,25 @@
 class CommandPool
 {
 private:
-	VkCommandPool _pool;
+	VkCommandPool _pool{ nullptr };
 	Device* _device;
 	VkCommandPoolCreateFlagBits _flags;
 	uint32_t _family_index;
-	bool _created;
 
 public:
 	CommandPool();
+	CommandPool(const CommandPool& cp);
+	~CommandPool();
 
 	void setDevice(Device* device);
 	void setFlags(VkCommandPoolCreateFlagBits flags);
 	void setQueueFamilyIndex(uint32_t family_index);
 	void create();
 	void reset();
-	void destroy();
 
 	const VkCommandPool& getPool() const;
+
+private:
+	void destroy();
 };
 

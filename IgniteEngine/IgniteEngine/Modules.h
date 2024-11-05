896c3fe875e776cfbcaea95714c1a8e577feec95
Moiup <mohammedbashir.m@gmail.com>
2024-11-05 11:02:22 +0100

    Activating and deactivating modules for debug purpose.

--- IgniteEngine/IgniteEngine/Modules.h
+++ IgniteEngine/IgniteEngine/Modules.h
@@ -3,19 +3,20 @@
 #include "Window.h"
 #include "Renderer.h"
 #include "Module.h"
-#include "TestModule.h"
-#include "GltfAndSkinningModule.h"
- #include "MediapipeModule.h"
-#include "MenuModule.h"
 #include "Camera.h"
 
-#include "DefaultModule.h"
-#include "DebugScene.h"
+#include "MenuModule.h"
+
+//#include "TestModule.h"
+// #include "MediapipeModule.h"
 #include "Bistro.h"
-#include "ComputeShaderTest.h"
-#include "ReadWriteImageMod.h"
-#include "Hand2D.h"
-#include "GreenScreen.h"
+//#include "ComputeShaderTest.h"
+//#include "ReadWriteImageMod.h"
+//#include "Hand2D.h"
+//#include "GltfAndSkinningModule.h"
+//#include "GreenScreen.h"
+#include "DebugScene.h"
+#include "DefaultModule.h"
 
 class Modules
 {
@@ -23,12 +24,12 @@ private:
 	MenuModule _menu_module;
 
 	//TestModule _test_module;
-	MediapipeModule _mediapipe_module;
-	//Bistro _bistro;
+	//MediapipeModule _mediapipe_module;
+	Bistro _bistro;
 	//ComputeShaderTest _compute_shader_test;
 	//ReadWriteImageMod _read_write_image_mod;
 	//Hand2D _hand2d;
-	GltfAndSkinningModule _gltf_and_skinning_module;
+	//GltfAndSkinningModule _gltf_and_skinning_module;
 
 	//GreenScreen _green_screen;
 

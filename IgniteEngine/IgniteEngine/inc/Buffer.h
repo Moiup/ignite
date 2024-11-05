7ace21c83e7526213e91619415797381f24b33c0
Moiup <mohammedbashir.m@gmail.com>
2024-11-05 10:46:47 +0100

    Removing the need of queue. Constructor to build a buffer. Templating.

--- IgniteEngine/IgniteEngine/inc/Buffer.h
+++ IgniteEngine/IgniteEngine/inc/Buffer.h
@@ -1,51 +1,188 @@
 #pragma once
 
-#include "Ressource.h"
-#include "Pointer.h"
-#include "ImageRessource.h"
-#include <unordered_map>
+#include "Buffer.hpp"
 
-class Buffer : public Ressource
+template<IGEBufferUsage U>
+Buffer<U>::Buffer() {
+	_nb_shared = new int32_t(1);
+}
+
+template<IGEBufferUsage U>
+Buffer<U>::Buffer(
+	Device* device,
+	VkDeviceSize size
+) :
+	Buffer<U>()
 {
-private:
-	VkBuffer _buffer{ nullptr };
-	VkBufferCreateInfo _buffer_info{};
-	VkDeviceSize _capacity{ 0 };
-	VkDeviceSize _size{ 0 };
+	_buffer_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	setPNext(nullptr);
+	setFlags(0);
+	setUsage(U);
+	setMemoryPropertyFlags(
+		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+		VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+	);
+	setSharingMode(VK_SHARING_MODE_EXCLUSIVE);
+	setQueueFamilyIndexCount(0);
+	setPQueueFamilyIndices(nullptr);
+	setDevice(device);
+	setSize(size);
+}
 
-public:
-	Buffer();
+template<IGEBufferUsage U>
+Buffer<U>::Buffer(const Buffer<U>& buf) {
+	*this = buf;
+}
 
-	void create();
-	void setValues(void* values);
-	void bind();
-	void destroy();
+template<IGEBufferUsage U>
+Buffer<U>::~Buffer() {
+	destroy();
+}
 
-	void setPNext(const void * p_next);
-	void setFlags(VkBufferCreateFlags flags);
-	void setSize(VkDeviceSize size);
-	void setCapacity(VkDeviceSize capacity);
-	void setUsage(VkBufferUsageFlags usage);
-	void setSharingMode(VkSharingMode sharing_mode);
-	void setQueueFamilyIndexCount(uint32_t family_index_count);
-	void setPQueueFamilyIndices(const uint32_t* p_queue_family_indices);
+template<IGEBufferUsage U>
+Buffer<U>& Buffer<U>::operator=(const Buffer<U>& buf) {
+	Ressource::operator=(buf);
+	_buffer = buf._buffer;
+	_buffer_info = buf._buffer_info;
+	_capacity = buf._capacity;
+	_size = buf._size;
+	_nb_shared = buf._nb_shared;
+	*_nb_shared += 1;
+	return *this;
+}
 
-	VkBuffer& getBuffer();
-	Pointer<uint8_t> getValues();
-	void getValues(Pointer<uint8_t> data);
+template<IGEBufferUsage U>
+void Buffer<U>::create() {
+	if (!_buffer_info.size) {
+		return;
+	}
+	createBuffer();
+	allocateMemory();
+	bind();
+}
 
-	uint32_t getSize();
-	uint32_t getCapacity();
-	
-	void* map();
-	void unmap();
+template<IGEBufferUsage U>
+void Buffer<U>::destroy() {
+	*_nb_shared -= 1;
+	if(*_nb_shared){
+		return;
+	}
+	delete _nb_shared;
 
-	// Copy image to buffer
-	void copy(ImageRessource& img);
+	if (!_buffer) {
+		return;
+	}
+	freeMemory();
+	destroyBuffer();
+}
 
-protected:
-	void createBuffer();
-	void destroyBuffer();
-	void getMemoryRequirements();
-};
+template<IGEBufferUsage U>
+void Buffer<U>::bind() {
+	VkResult vk_result = vkBindBufferMemory(
+		_device->getDevice(),
+		_buffer,
+		_memory,
+		0
+	);
 
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error: failed binding the buffer!");
+	}
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setPNext(const void* p_next) {
+	_buffer_info.pNext = p_next;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setFlags(VkBufferCreateFlags flags) {
+	_buffer_info.flags = flags;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setSize(VkDeviceSize size) {
+	if (size > _capacity) {
+		_capacity = size;
+		_buffer_info.size = _capacity;
+	}
+	_size = size;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setCapacity(VkDeviceSize capacity) {
+	if (capacity < _buffer_info.size) {
+		std::cerr << "Buffer<U>::setCapacity: Error! Capacity must be superior to size." << std::endl;
+		return;
+	}
+
+	_capacity = capacity;
+	_buffer_info.size = _capacity;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setUsage(VkBufferUsageFlags usage) {
+	_buffer_info.usage = usage;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setSharingMode(VkSharingMode sharing_mode) {
+	_buffer_info.sharingMode = sharing_mode;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setQueueFamilyIndexCount(uint32_t family_index_count) {
+	_buffer_info.queueFamilyIndexCount = family_index_count;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::setPQueueFamilyIndices(const uint32_t* p_queue_family_indices) {
+	_buffer_info.pQueueFamilyIndices = p_queue_family_indices;
+}
+
+template<IGEBufferUsage U>
+VkBuffer& Buffer<U>::getBuffer() {
+	return _buffer;
+}
+
+template<IGEBufferUsage U>
+uint32_t Buffer<U>::getSize() {
+	return _size;
+}
+
+template<IGEBufferUsage U>
+uint32_t Buffer<U>::getCapacity() {
+	return _capacity;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::createBuffer() {
+	VkResult vk_result = vkCreateBuffer(
+		_device->getDevice(),
+		&_buffer_info,
+		nullptr,
+		&_buffer
+	);
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error: failed creating the buffer!");
+	}
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::destroyBuffer() {
+	vkDestroyBuffer(
+		_device->getDevice(),
+		_buffer,
+		nullptr
+	);
+	_buffer = nullptr;
+}
+
+template<IGEBufferUsage U>
+void Buffer<U>::getMemoryRequirements() {
+	vkGetBufferMemoryRequirements(
+		_device->getDevice(),
+		_buffer,
+		&_memory_req
+	);
+}
---
+++ IgniteEngine/IgniteEngine/inc/Buffer.hpp
@@ -0,0 +1,57 @@
+#include "Ressource.h"
+#include "Pointer.h"
+#include <unordered_map>
+
+template <IGEBufferUsage U>
+class Buffer : public Ressource
+{
+protected:
+	VkBuffer _buffer{ nullptr };
+	VkBufferCreateInfo _buffer_info{};
+	VkDeviceSize _capacity{ 0 };
+	VkDeviceSize _size{ 0 };
+
+	int32_t* _nb_shared{nullptr};
+
+	Buffer();
+	Buffer(
+		Device* device,
+		VkDeviceSize size
+	);
+
+public:
+	Buffer(const Buffer<U>& buf);
+	~Buffer();
+
+	Buffer<U>& operator=(const Buffer<U>& buf);
+
+	void setValues(void* values);
+	void bind();
+	void destroy();
+
+	VkBuffer& getBuffer();
+	Pointer<uint8_t> getValues();
+	void setValues(Pointer<uint8_t> data);
+
+	uint32_t getSize();
+	uint32_t getCapacity();
+
+	void* map();
+	void unmap();
+
+protected:
+	void create();
+
+	void setPNext(const void* p_next);
+	void setFlags(VkBufferCreateFlags flags);
+	void setSize(VkDeviceSize size);
+	void setCapacity(VkDeviceSize capacity);
+	void setUsage(VkBufferUsageFlags usage);
+	void setSharingMode(VkSharingMode sharing_mode);
+	void setQueueFamilyIndexCount(uint32_t family_index_count);
+	void setPQueueFamilyIndices(const uint32_t* p_queue_family_indices);
+
+	void createBuffer();
+	void destroyBuffer();
+	void getMemoryRequirements();
+};

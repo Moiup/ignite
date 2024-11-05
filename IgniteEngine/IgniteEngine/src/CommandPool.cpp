07597b3a347666682e2b7087204bdcf243605cd3
Moiup <mohammedbashir.m@gmail.com>
2024-11-05 11:23:08 +0100

    Allocating and freeing command buffers. Storing a device. Managing CommandPool lifetime.

--- IgniteEngine/IgniteEngine/src/CommandPool.cpp
+++ IgniteEngine/IgniteEngine/src/CommandPool.cpp
@@ -2,23 +2,42 @@
 
 CommandPool::CommandPool() :
 	_pool{nullptr},
+	_device{nullptr},
 	_flags{},
-	_family_index{}
+	_family_index{},
+	_shared_count{nullptr}
 {
-	;
+	_command_buffers = new std::vector<VkCommandBuffer>;
+	_shared_count = new int(1);
 }
 
-CommandPool::CommandPool(const CommandPool& cp) :
-	_pool{ cp._pool },
-	_device{ cp._device },
-	_flags{ cp._flags },
-	_family_index{ cp._family_index }
+CommandPool::CommandPool(const CommandPool& cp)
 {
+	*this = cp;
+}
 
+CommandPool& CommandPool::operator=(const CommandPool& cp) {
+	_pool = { cp._pool };
+	_device = { cp._device };
+	_flags = { cp._flags };
+	_family_index = { cp._family_index };
+	_command_buffers = { cp._command_buffers };
+	_shared_count = { cp._shared_count };
+	*_shared_count += 1;
+
+	return *this;
+}
+
+bool CommandPool::operator==(const CommandPool& cp) {
+	return _pool == cp._pool;
 }
 
 CommandPool::~CommandPool() {
-	destroy();
+	*_shared_count -= 1;
+	if (!*_shared_count) {
+		destroy();
+		delete _shared_count;
+	}
 }
 
 void CommandPool::setDevice(Device* device) {
@@ -49,10 +68,14 @@ void CommandPool::create() {
 	if (vk_result != VK_SUCCESS) {
 		throw std::runtime_error("Error: failed creating command pool!");
 	}
-
 }
 
 void CommandPool::reset() {
+	if (_command_buffers->empty()) {
+		return;
+	}
+
+	resetCommandBuffers();
 	VkResult vk_result = vkResetCommandPool(
 		_device->getDevice(),
 		_pool,
@@ -60,10 +83,44 @@ void CommandPool::reset() {
 	);
 }
 
+const VkCommandPool& CommandPool::getPool() const {
+	return _pool;
+}
+
+CommandBuffer& CommandPool::newCommandBuffer() {
+	VkCommandBuffer cmd_buf{ nullptr };
+	VkCommandBufferAllocateInfo info{};
+	info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	info.pNext = nullptr;
+	info.commandPool = _pool;
+	info.commandBufferCount = 1;
+	info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+	VkResult vk_result = vkAllocateCommandBuffers(
+		_device->getDevice(),
+		&info,
+		&cmd_buf
+	);
+
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error: failed allocating command buffer!");
+	}
+
+	CommandBuffer command_buffer;
+	command_buffer._command_buffer = cmd_buf;
+	_command_buffers->push_back(cmd_buf);
+
+	return command_buffer;
+}
+
 void CommandPool::destroy() {
 	if (!_pool) {
 		return;
 	}
+
+	reset();
+	
+	delete _command_buffers;
 
 	vkDestroyCommandPool(
 		_device->getDevice(),
@@ -72,6 +129,12 @@ void CommandPool::destroy() {
 	);
 }
 
-const VkCommandPool& CommandPool::getPool() const {
-	return _pool;
+void CommandPool::resetCommandBuffers() {
+	vkFreeCommandBuffers(
+		_device->getDevice(),
+		_pool,
+		_command_buffers->size(),
+		_command_buffers->data()
+	);
+	_command_buffers->clear();
 }

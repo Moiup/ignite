1d283a2aa978ab7d5023992e284ec9d5d00da5af
Moiup <mohammedbashir.m@gmail.com>
2024-11-05 11:28:00 +0100

    Setter and getter for index type.

--- IgniteEngine/IgniteEngine/inc/IndexBufferInfo.h
+++ IgniteEngine/IgniteEngine/inc/IndexBufferInfo.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <iostream>
+#include <vulkan/vulkan.h>
 
 class IndexBufferInfo
 {
 private:
 	uint32_t _nb_elem;
+	VkIndexType _index_type;
 
 public:
 	IndexBufferInfo();
 
 	void setNbElem(uint32_t nb_elem);
+	void setIndexType(VkIndexType index_type);
 
 	const uint32_t getNbElem();
+	const VkIndexType getIndexType() const;
 };
 
--- IgniteEngine/IgniteEngine/src/IndexBufferInfo.cpp
+++ IgniteEngine/IgniteEngine/src/IndexBufferInfo.cpp
@@ -10,6 +10,15 @@ void IndexBufferInfo::setNbElem(uint32_t nb_elem) {
 	_nb_elem = nb_elem;
 }
 
+
+void IndexBufferInfo::setIndexType(VkIndexType index_type) {
+	_index_type = index_type;
+}
+
 const uint32_t IndexBufferInfo::getNbElem() {
 	return _nb_elem;
+}
+
+const VkIndexType IndexBufferInfo::getIndexType() const {
+	return _index_type;
 }

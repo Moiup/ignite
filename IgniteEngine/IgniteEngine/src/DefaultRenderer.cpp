0ec55b42037ee9fb4d8138f689f20a6962123c2f
Moiup <mohammedbashir.m@gmail.com>
2024-11-05 11:40:12 +0100

    Updated taking into account the new engine Buffers, textures...

--- IgniteEngine/IgniteEngine/src/DefaultRenderer.cpp
+++ IgniteEngine/IgniteEngine/src/DefaultRenderer.cpp
@@ -16,7 +16,6 @@ void DefaultRenderer::create() {
 
 void DefaultRenderer::destroy() {
 	_swapchain.destroy();
-	_depth_buffer.destroy();
 	for (GraphicsPipeline& gp : _graphics_pipelines) {
 		gp.destroy();
 	}
@@ -53,9 +52,8 @@ void DefaultRenderer::render() {
 	else {
 		_is_not_first_frame = true;
 	}
+	_graphics_queues_in_flight[_present_frame].wait();
 	
-	_graphics_queues_in_flight[_current_frame].wait();
-
 	vk_result = vkAcquireNextImageKHR(
 		(*_graphics_queues)[0].getDevice()->getDevice(),
 		_swapchain.getSwapchain(),
@@ -68,7 +66,7 @@ void DefaultRenderer::render() {
 		throw std::runtime_error("Error: failed acquiring next image!");
 	}
 
-	CommandBuffer cmd_buf = _graphics_queues_in_flight[_current_frame].allocateCommandBuffer();
+	CommandBuffer cmd_buf = _graphics_queues_in_flight[_current_frame].newCommandBuffer();
 
 	cmd_buf.reset();
 	cmd_buf.begin();
@@ -115,26 +113,27 @@ void DefaultRenderer::render() {
 		const VkDeviceSize buff_offset[1] = { 0 };
 		GraphicShader* gs = gp.getShader();
 		// Vertex Buffers
-		std::unordered_map<std::string, std::vector<VertexBuffer* >>& vbs_map = gs->getVertexBuffers();
-		std::vector<VertexBuffer*>& vbs = vbs_map.begin()->second;
+		std::unordered_map<std::string, std::vector<Buffer<IGEBufferUsage::vertex_buffer>* >>& vbs_map = gs->getVertexBuffers();
+		std::vector<Buffer<IGEBufferUsage::vertex_buffer>*>& vbs = vbs_map.begin()->second;
 		// Index Buffers
-		std::unordered_map<std::string, std::vector<IndexBuffer*>>& ibs_map = gs->getIndexBuffers();
-		std::vector<IndexBuffer*>& ibs = ibs_map.begin()->second;
+		std::unordered_map<std::string, std::vector<Buffer<IGEBufferUsage::index_buffer>*>>& ibs_map = gs->getIndexBuffers();
+		const IndexBufferInfo& ibs_info = gp.getShader()->getIndexBufferInfo(ibs_map.begin()->first);
+		std::vector<Buffer<IGEBufferUsage::index_buffer>*>& ibs = ibs_map.begin()->second;
 		uint32_t nb_buff = vbs.size();
 		for (uint32_t i = 0; i < nb_buff; i++) {
 			// Index buffer binding
-			IndexBuffer* ib = ibs[i];
+			Buffer<IGEBufferUsage::index_buffer>* ib = ibs[i];
 			cmd_buf.bindIndexBuffer(
 				ib->getBuffer(),
 				buff_offset[0],
-				ib->getIndexType()
+				ibs_info.getIndexType()
 			);
 
 			// Vertex buffers binding
 			for (auto& vbs_pair : vbs_map) {
 				std::string name = vbs_pair.first;
-				std::vector<VertexBuffer*>& vertex_buffers = vbs_pair.second;
-				VertexBuffer* vertex_buffer = vertex_buffers[i];
+				std::vector<Buffer<IGEBufferUsage::vertex_buffer>*>& vertex_buffers = vbs_pair.second;
+				Buffer<IGEBufferUsage::vertex_buffer>* vertex_buffer = vertex_buffers[i];
 				cmd_buf.bindVertexBuffer(
 					gs->getVertexBufferInfo(name).getFirstBinding(),
 					1,
@@ -173,149 +172,15 @@ void DefaultRenderer::render() {
 		&_sem_render_ends[_current_frame]
 	);
 
-	_swapchain.getImages()[_available_img].setQueue(&_graphics_queues_in_flight[_current_frame]);
-
-	//if (_current_frame == 1) {
-	//	
-	//	Pixels pix{};
-	//	pix.setPixels(
-	//		_swapchain.getImages()[_available_img].getImageExtentWidth(),
-	//		_swapchain.getImages()[_available_img].getImageExtentHeight());
-
-	//	Image& img = _swapchain.getImages()[_available_img];
-	//	img.flushPixels(pix);
-
-	//	pix.saveFile("../assets/ouioui.png");
-
-	//}
-
-
-	//// Copying depth image to a buffer
-	//// Staging buffer
-	//Buffer staging_buffer{};
-	//staging_buffer.setLogicalDevice((VkDevice*)_logical_device->getDevice());
-	//staging_buffer.setMemoryProperties(_gpu->getMemoryProperties());
-	//staging_buffer.setFlags(0);
-	//staging_buffer.setPNext(nullptr);
-	//staging_buffer.setQueueFamilyIndexCount(0);
-	//staging_buffer.setPQueueFamilyIndices(nullptr);
-	//staging_buffer.setSize(_window->getHeight() * _window->getWidth() * 1 * sizeof(uint64_t));
-	//staging_buffer.setUsage(VK_BUFFER_USAGE_TRANSFER_DST_BIT);
-	//staging_buffer.setSharingMode(VK_SHARING_MODE_EXCLUSIVE);
-	//staging_buffer.create();
-
-	//staging_buffer.allocateMemory(
-	//	VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
-	//);
-	//staging_buffer.bind();
-
-	//std::vector<VkBufferImageCopy> buffer_image_copy_arr{};
-
-	//// To do for each mip level
-	//// (To start, we consider only the original level -> 0)
-	//VkBufferImageCopy buffer_image_copy{};
-	//buffer_image_copy.bufferOffset = 0;
-	//buffer_image_copy.imageSubresource.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
-	//buffer_image_copy.imageSubresource.mipLevel = 0;
-	//buffer_image_copy.imageSubresource.baseArrayLayer = 0;
-	//buffer_image_copy.imageSubresource.layerCount = 1;
-	//buffer_image_copy.imageExtent.width = _window->getWidth();
-	//buffer_image_copy.imageExtent.height = _window->getHeight();
-	//buffer_image_copy.imageExtent.depth = 1;
-	//buffer_image_copy_arr.push_back(buffer_image_copy);
-
-	//// Preparing the transfer with the image memory barrier
-	//CommandBuffer copy_cmd{};
-	//copy_cmd.setLogicalDevice((VkDevice*)_logical_device->getDevice());
-	//copy_cmd.setCommandPool((VkCommandPool*)&_command_pool->getPool());
-	//copy_cmd.setLevel(VK_COMMAND_BUFFER_LEVEL_PRIMARY);
-	//copy_cmd.create();
-	//copy_cmd.begin();
-
-	//VkBufferMemoryBarrier buffer_memory_barrier{};
-	//buffer_memory_barrier.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
-	//buffer_memory_barrier.pNext = nullptr;
-	//buffer_memory_barrier.srcAccessMask = 0;
-	//buffer_memory_barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-	//buffer_memory_barrier.buffer = staging_buffer.getBuffer();
-	//buffer_memory_barrier.offset = 0;
-	//buffer_memory_barrier.size = _window->getHeight() * _window->getWidth() * 1 * sizeof(char);
-
-	//VkImageSubresourceRange depth_subresource_range_frame{};
-	//depth_subresource_range_frame.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
-	//depth_subresource_range_frame.baseMipLevel = 0;
-	//depth_subresource_range_frame.levelCount = 1;
-	//depth_subresource_range_frame.baseArrayLayer = 0;
-	//depth_subresource_range_frame.layerCount = 1;
-
-	//VkImageMemoryBarrier depth_memory_barrier_frame{};
-	//depth_memory_barrier_frame.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-	//depth_memory_barrier_frame.pNext = nullptr;
-	//depth_memory_barrier_frame.srcAccessMask = 0;
-	//depth_memory_barrier_frame.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-	//depth_memory_barrier_frame.oldLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-	//depth_memory_barrier_frame.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-	//depth_memory_barrier_frame.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-	//depth_memory_barrier_frame.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-	//depth_memory_barrier_frame.image = _depth_buffer.getImage();
-	//depth_memory_barrier_frame.subresourceRange = depth_subresource_range_frame;
-
-	//copy_cmd.pipelineBarrier(
-	//	VK_PIPELINE_STAGE_HOST_BIT,
-	//	VK_PIPELINE_STAGE_TRANSFER_BIT,
-	//	0,
-	//	0, nullptr,
-	//	1, &buffer_memory_barrier,
-	//	1, &depth_memory_barrier_frame
+	//_present_queues_in_flight[_current_frame].present(
+	//	1,
+	//	&_sem_render_ends[_current_frame],
+	//	1,
+	//	&_swapchain.getSwapchain(),
+	//	&_available_img
 	//);
 
-	//depth_memory_barrier_frame.srcAccessMask = 0;
-	//depth_memory_barrier_frame.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-	//depth_memory_barrier_frame.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	//depth_memory_barrier_frame.newLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-	//vkCmdCopyImageToBuffer(
-	//	*copy_cmd.getCommandBuffer(),
-	//	_depth_buffer.getImage(),
-	//	VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
-	//	staging_buffer.getBuffer(),
-	//	buffer_image_copy_arr.size(),
-	//	buffer_image_copy_arr.data()
-	//);
-
-	//copy_cmd.pipelineBarrier(
-	//	VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-	//	VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-	//	0,
-	//	0, nullptr,
-	//	0, nullptr,
-	//	1, &depth_memory_barrier_frame
-	//);
-
-	//copy_cmd.end();
-	//copy_cmd.flush(_logical_device->getDefaultQueue());
-	//copy_cmd.free();
-
-	//float* val = (float*)staging_buffer.getValues();
-
-	//std::vector<glm::vec4> depth;
-	//depth.resize(_window->getWidth() * _window->getHeight());
-
-	//for (uint32_t i = 0; i < _window->getWidth() * _window->getHeight(); i++) {
-	//	float f = val[i] * 255;
-	//	depth[i].r = f;
-	//	depth[i].g = f;
-	//	depth[i].b = f;
-	//	depth[i].a = 255;
-	//}
-
-	//Texture tex;
-	//tex.setPixels(depth, _window->getWidth(), _window->getHeight());
-	//tex.writeFile("../assets/test_test.bmp");
-
-	//delete val;
-
-	//staging_buffer.destroy();
+	//_graphics_queues_in_flight[_current_frame].wait();
 
 	_present_frame = _current_frame;
 	_current_frame = (_current_frame + 1) % _nb_frame;
@@ -328,13 +193,11 @@ Image& DefaultRenderer::getCurrentFrame() {
 void DefaultRenderer::configureQueues() {
 	for (uint32_t i = 0; i < _nb_frame; i++) {
 		Queue gq = (*_graphics_queues)[0];
-		gq.addCommandPool();
 		_graphics_queues_in_flight.push_back(
 			gq
 		);
 
 		Queue pq = (*_present_queues)[0];
-		pq.addCommandPool();
 		_present_queues_in_flight.push_back(
 			pq
 		);
@@ -403,8 +266,8 @@ void DefaultRenderer::createSwapchain() {
 		throw std::runtime_error("Error: no proper format found!");
 	}
 
-	_swapchain.setQueue(
-		&(*_graphics_queues)[0]
+	_swapchain.setDevice(
+		_device
 	);
 	_swapchain.setSurface(
 		_window->getSurface()
@@ -421,21 +284,37 @@ void DefaultRenderer::createSwapchain() {
 		{(*_graphics_queues)[0].getFamilyIndex()}
 	);
 	_swapchain.create();
+
+	for (Image& img : _swapchain.getImages()) {
+		(*_graphics_queues)[0].changeLayout(
+			img,
+			VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+		);
+	}
+	(*_graphics_queues)[0].submit();
+	(*_graphics_queues)[0].wait();
 }
 
 void DefaultRenderer::createDepthBuffer() {
-	_depth_buffer.setQueue(
-		&(*_graphics_queues)[0]
-	);
-	_depth_buffer.setImageWidthHeight(
+	_depth_buffer = DepthBuffer(
+		_device,
 		_window->getWidthInPixel(),
-		_window->getHeightInPixel()
+		_window->getHeightInPixel(),
+		{ (*_graphics_queues)[0].getFamilyIndex() }
 	);
 
-	_depth_buffer.setImageQueueFamilyIndices(
-		{(*_graphics_queues)[0].getFamilyIndex()}
-	);
-	_depth_buffer.create();
+	//_depth_buffer.setDevice(
+	//	_device
+	//);
+	//_depth_buffer.setImageWidthHeight(
+	//	_window->getWidthInPixel(),
+	//	_window->getHeightInPixel()
+	//);
+
+	//_depth_buffer.setImageQueueFamilyIndices(
+	//	{(*_graphics_queues)[0].getFamilyIndex()}
+	//);
+	//_depth_buffer.create();
 }
 
 void DefaultRenderer::createGraphicsPipeline() {
@@ -448,7 +327,8 @@ void DefaultRenderer::createGraphicsPipeline() {
 			continue;
 		}
 		
-		GraphicsPipeline gp{};
+		_graphics_pipelines.push_back(GraphicsPipeline());
+		GraphicsPipeline& gp = _graphics_pipelines.back();
 		gp.setShader(shader);
 		gp.setNbFrame(_nb_frame);
 		gp.setSwapchain(&_swapchain);
@@ -457,7 +337,6 @@ void DefaultRenderer::createGraphicsPipeline() {
 		gp.setTopology(shader->topology());
 		//gp.setCullMode(VK_CULL_MODE_BACK_BIT);
 		gp.create();
-		_graphics_pipelines.push_back(gp);
 	}
 }
 

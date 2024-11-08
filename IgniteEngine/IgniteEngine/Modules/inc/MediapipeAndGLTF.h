7c27977de99305aa1af734711314bf2b61343c21
Moiup <mohammedbashir.m@gmail.com>
2024-11-08 11:36:41 +0100

    Module to animate a hand with mediapipe.

---
+++ IgniteEngine/IgniteEngine/Modules/inc/MediapipeAndGLTF.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <thread>
+#include "EngineEntity.h"
+#include "Module.h"
+#include "Object3D.h"
+#include "Renderer.h"
+
+#include "ESocket/EServerSocket.h"
+//#include "InitNetworkProtocole.h"
+//#include "MocapMessageProtocolNetwork.h"
+
+#include "SkeletonDebug.h"
+
+#include <string>
+
+namespace mdph {
+    struct DimMsg {
+        uint32_t _width;
+        uint32_t _height;
+    };
+
+    struct OkMsg {
+        uint8_t _is_ok;
+    };
+
+    struct Landmarks {
+        uint32_t _handedness; // 0 -> right; 1 -> left
+        glm::vec3 _landmarks[2][21];
+        glm::vec3 _world_landmarks[2][21];
+    };
+}
+
+class MediapipeAndGLTF : public Module
+{
+public:
+    const std::string SERVER_ADDRESS{ "127.0.0.1" };
+    const std::string SERVER_PORT{ "8080" };
+    const float RAD_TO_DEG = 180 / M_PI;
+private:
+    EServerSocket _server_socket;
+    EStream _mediapipe_stream;
+    std::thread _network_thread;
+
+    Mesh _cube;
+    Mesh _cross_mesh;
+    std::vector<Object3D> _cross_objs;
+    std::vector<Material> _cross_material;
+    std::vector<uint32_t> _cross_material_indices;
+    float _size;
+
+    LoadedObjectInfo _hand_obj_info;
+    Object3D _hand;
+
+    const uint32_t _wrist_index = 21;
+
+    std::vector<std::vector<Object3D>> _hands;
+    std::vector<Object3D> _right_hand;
+    std::vector<Object3D> _wrists;
+    std::vector<glm::mat4> _alignment_matrices;
+    std::vector<glm::mat4> _transform_matrices;
+
+
+    uint32_t _frame_i;
+    std::vector<glm::vec3> _wrist_pos;
+
+
+    // -- HAND SHADER --
+    GraphicShader _lbs_shader{};
+    // Buffers
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _coord_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _object_id_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _material_indices_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _uv_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _joints_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::vertex_buffer> _weights_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::index_buffer> _index_buffer_hand{};
+    // Uniform buffer
+    StagingBuffer<IGEBufferUsage::storage_buffer> _obj_tr_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::storage_buffer> _joint_tr_buffer_hand{};
+    StagingBuffer<IGEBufferUsage::storage_buffer> _materials_buffer_hand{};
+    Sampler _sampler_hand{};
+
+    glm::mat4 _camera;
+
+public:
+    MediapipeAndGLTF();
+
+    void init();
+    void start();
+    void update();
+    void close();
+
+    void networkProcess();
+
+private:
+    void landmarksToLocal(const mdph::Landmarks& landmarks);
+    void landmarksRotationMatrices(const mdph::Landmarks& landmarks);
+
+    glm::mat4 findRotationMatrix(
+        const glm::vec3& from,
+        const glm::vec3& to
+    );
+
+    /**
+    * Compute the rotation matrix to align vector 'from' on vector 'to'
+    */
+    glm::mat3 alignVectorMatrix(const glm::vec3& from, const glm::vec3& to);
+
+    glm::vec3 vectorAngle(glm::vec3 A, glm::vec3 B);
+
+    std::vector<glm::vec3> cube();
+    std::vector<uint32_t> cubeIndex();
+
+    void createShaderHand();
+};
+

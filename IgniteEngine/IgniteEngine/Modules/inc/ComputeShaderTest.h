d9c2e829716a9f498cab1f61f3c683a147fc8a54
Moiup <mohammedbashir.m@gmail.com>
2024-12-10 15:27:12 +0100

    Commenting for debug purpose.

--- IgniteEngine/IgniteEngine/Modules/inc/ComputeShaderTest.h
+++ IgniteEngine/IgniteEngine/Modules/inc/ComputeShaderTest.h
@@ -1,24 +1,24 @@
-#pragma once
-
-#include "Module.h"
-#include "DefaultDispatcher.h"
-
-class ComputeShaderTest : public Module
-{
-private:
-	ComputeShader _compute_shader;
-
-	StorageBuffer _input_data;
-	StorageBuffer _output_data;
-	ComputePipeline _compute_pipeline;
-	DefaultDispatcher _dispatcher;
-
-public:
-	ComputeShaderTest();
-
-	void init();
-	void start();
-	void update();
-	void close();
-};
-
+//#pragma once
+//
+//#include "Module.h"
+//#include "DefaultDispatcher.h"
+//
+//class ComputeShaderTest : public Module
+//{
+//private:
+//	ComputeShader _compute_shader;
+//
+//	StorageBuffer _input_data;
+//	StorageBuffer _output_data;
+//	ComputePipeline _compute_pipeline;
+//	DefaultDispatcher _dispatcher;
+//
+//public:
+//	ComputeShaderTest();
+//
+//	void init();
+//	void start();
+//	void update();
+//	void close();
+//};
+//
--- IgniteEngine/IgniteEngine/Modules/inc/GreenScreen.h
+++ IgniteEngine/IgniteEngine/Modules/inc/GreenScreen.h
@@ -1,45 +1,45 @@
-#pragma once
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_sdl2.h"
-#include "imgui/backends/imgui_impl_sdlrenderer2.h"
-
-#include "Module.h"
-#include "DefaultDispatcher.h"
-#include "RendererDisplayImage.h"
-
-struct GreenScreenPC {
-    glm::vec4 _color;
-    uint32_t _width;
-    uint32_t _height;
-    float _hardness;
-};
-
-class GreenScreen : public Module
-{
-private:
-    Texture _input;
-    Texture _output;
-
-    GreenScreenPC _push_constant;
-
-    ComputeShader _green_screen_comp;
-    ComputePipeline _compute_pipeline;
-    DefaultDispatcher _dispatcher;
-
-    Pixels _img;
-
-    WindowSurface _display_window{};
-    RendererDisplayImage _rdi;
-
-public:
-    GreenScreen() = default;
-
-    void init();
-    void start();
-    void update();
-    void close();
-
-private:
-    void createShader();
-};
-
+//#pragma once
+//#include "imgui/imgui.h"
+//#include "imgui/backends/imgui_impl_sdl2.h"
+//#include "imgui/backends/imgui_impl_sdlrenderer2.h"
+//
+//#include "Module.h"
+//#include "DefaultDispatcher.h"
+//#include "RendererDisplayImage.h"
+//
+//struct GreenScreenPC {
+//    glm::vec4 _color;
+//    uint32_t _width;
+//    uint32_t _height;
+//    float _hardness;
+//};
+//
+//class GreenScreen : public Module
+//{
+//private:
+//    Texture _input;
+//    Texture _output;
+//
+//    GreenScreenPC _push_constant;
+//
+//    ComputeShader _green_screen_comp;
+//    ComputePipeline _compute_pipeline;
+//    DefaultDispatcher _dispatcher;
+//
+//    Pixels _img;
+//
+//    WindowSurface _display_window{};
+//    RendererDisplayImage _rdi;
+//
+//public:
+//    GreenScreen() = default;
+//
+//    void init();
+//    void start();
+//    void update();
+//    void close();
+//
+//private:
+//    void createShader();
+//};
+//

82e3137352983d7589b492aa6c38faae9c84b258
Moiup <mohammedbashir.m@gmail.com>
2025-01-02 11:38:51 +0100

    Launching modules.

--- IgniteEngine/IgniteEngine/IgniteEngine.cpp
+++ IgniteEngine/IgniteEngine/IgniteEngine.cpp
@@ -1,9 +1,16 @@
 #include "EngineApp.h"
 #include <SDL.h>
 
+struct LaunchedModules {
+    MenuModule menu;
+    Bistro bistro;
+    DefaultModule default_module;
+};
+
 int main(int argc, char* argv[]) {
     EngineApp app{};
-
+    LaunchedModules modules{};
+ 
     app.launch();
 
     return 0;

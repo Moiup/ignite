537e6cdb9b02b2ff9974f402bcf3160aa55206e3
Moiup <mohammedbashir.m@gmail.com>
2025-01-02 12:08:53 +0100

    Removed commented code.

--- IgniteEngine/IgniteEngine/Modules.h
+++ IgniteEngine/IgniteEngine/Modules.h
@@ -19,29 +19,3 @@
 #include "DebugScene.h"
 #include "DefaultInit.h"
 #include "DefaultRenderingModule.h"
-
-//class Modules
-//{
-//private:
-//	//MenuModule _menu_module;
-//
-//	//TestModule _test_module;
-//	//MediapipeModule _mediapipe_module;
-//	//MediapipeAndGLTF _mediapipe_and_gltf;
-//	//Bistro _bistro;
-//	//ComputeShaderTest _compute_shader_test;
-//	//ReadWriteImageMod _read_write_image_mod;
-//	//Hand2D _hand2d;
-//	//GltfAndSkinningModule _gltf_and_skinning_module;
-//
-//	//GreenScreen _green_screen;
-//
-//	//DebugScene _debug_scene;
-//	//DefaultModule _default_module; // Must be the last module of the list
-//
-//public:
-//	Modules();
-//
-//	void init();
-//};
-

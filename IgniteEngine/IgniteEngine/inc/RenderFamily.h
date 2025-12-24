22d6ec9bd48d2140522e34d489a6e81407e81d32
Moiup <mohammedbashir.m@gmail.com>
2025-12-24 20:15:36 +0100

    Replacing rendering with RenderFamily.

---
+++ IgniteEngine/IgniteEngine/inc/RenderFamily.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "WindowSurface.h"
+#include "CommandPool.h"
+#include "CommandBuffer.h"
+#include "Queue.h"
+#include "GraphicShader.h"
+#include "GraphicsPipeline.h"
+
+class RenderFamily {
+	// void _n;
+};
+
---
+++ IgniteEngine/IgniteEngine/src/RenderFamily.cpp
@@ -0,0 +1,1 @@
+#include "RenderFamily.h"

1139af155be045e83365ecd705aa0ec40571423d
Moiup <mohammedbashir.m@gmail.com>
2025-12-29 17:03:33 +0100

    Initializing attributes with default values.

--- IgniteEngine/IgniteEngine/inc/CameraScene.h
+++ IgniteEngine/IgniteEngine/inc/CameraScene.h
@@ -6,17 +6,17 @@
 class CameraScene : public EngineEntity
 {
 private:
-	PerspectiveCamera _perspective_camera;
-	Camera* _camera;
+	PerspectiveCamera _perspective_camera{};
+	Camera* _camera{nullptr};
 
-	bool _was_middle_clicked;
-	bool _is_middle_clicked;
-	glm::vec2 _middle_prev_mouse;
+	bool _was_middle_clicked{false};
+	bool _is_middle_clicked{false};
+	glm::vec2 _middle_prev_mouse{};
 	
-	bool _was_right_clicked;
-	bool _is_right_clicked;
-	glm::vec2 _right_prev_mouse;
-	glm::mat4 _mvp;
+	bool _was_right_clicked{false};
+	bool _is_right_clicked{false};
+	glm::vec2 _right_prev_mouse{};
+	glm::mat4 _mvp{};
 
 public:
 	CameraScene();
